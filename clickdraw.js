9e241b52566202ca4f50dc4a985b8e7ecc2c52a1
Taylor Wong <Taylor Wong>
2018-03-09 00:27:08 -0500

    shanlau finished

--- clickdraw.js
+++ clickdraw.js
@@ -12,59 +12,50 @@ var height = svg.getAttribute("height");
 
 var clear = document.getElementById("clear");
 
-var dot = {
-    
-    setColor function(c){
-	dot.setAttribute("fill",c);
-    };
-};
-
-//clears the screen and resets the prevX and prevY variables
+// clear screen to white
 var clearCallBack = function(e){
     while(svg.firstChild){
-	svg.removeChild(svg.firstChild);
+	     svg.removeChild(svg.firstChild);
     }
 };
 
-//draws a dot, returns the x and y coordinates in a list
-var drawDot = function(e){
-    var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-    c.setAttribute("cx", e.offsetX);
-    c.setAttribute("cy", e.offsetY);
-    c.setAttribute("r", 10);
-    c.setAttribute("fill", "red");
-    c.setAttribute("stroke", "black");
-    c.addEventListener("click", changeColor);
-    svg.appendChild(c);
+var dotCallBack = function(e){
+    drawDot(e.offsetX, e.offsetY, 25, "blue");
     e.stopPropagation();
+}
+
+// draws a dot
+var drawDot = function(x, y, r, color){
+    var c = {
+      cx : x,
+      cy : y,
+      radius: r,
+      fill : color,
+      circle : document.createElementNS("http://www.w3.org/2000/svg", "circle"),
+      update : function(e){
+          if (this.getAttribute("fill") == "blue"){
+            this.setAttribute("fill", "red");
+          } else {
+            var x = Math.random() * width - r;
+            var y = Math.random() * height - r;
+            this.setAttribute("cx", x);
+            this.setAttribute("cy", y);
+          }
+          e.stopPropagation();
+      },
+      display : function(){
+          svg.appendChild(this.circle);
+      }
+    };
+    c.circle.setAttribute("cx", x);
+    c.circle.setAttribute("cy", y);
+    c.circle.setAttribute("r", r);
+    c.circle.setAttribute("fill", color);
+    c.circle.setAttribute("stroke", "black");
+    c.circle.addEventListener("click", c.update);
+    c.display();
 };
-
-//toggles the color between red and blue
-var changeColor = function(e){
-    if(this.getAttribute("fill") == "red"){
-        this.setAttribute("fill", "blue");
-    }
-    else{
-	console.log("delete");
-	svg.removeChild(this);
-	randCircle();
-    }
-    e.stopPropagation();
-}
-
-//draws a circle at random coordinates
-var randCircle = function(e){
-    var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-    c.setAttribute("cx", Math.random() * (width - 10));
-    c.setAttribute("cy", Math.random() * (height - 10));
-    c.setAttribute("r", 10);
-    c.setAttribute("fill", "red");
-    c.setAttribute("stroke", "black");
-    c.addEventListener("click", changeColor);
-    svg.appendChild(c);
-}
 
 //add the event listeners
-svg.addEventListener("click", drawDot);
+svg.addEventListener("click", dotCallBack);
 clear.addEventListener("click", clearCallBack);
-
